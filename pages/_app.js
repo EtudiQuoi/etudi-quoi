8f3c61e9bf8e2e6a82c8449b3b9b3d4f0eb42009
NicoooM <nmarsan33000@gmail.com>
2022-03-22 16:29:40 +0100

    progressive web app

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,41 @@
 import { globalStyles, lightTheme } from "../shared/styles";
+import Head from "next/head";
 import { ThemeProvider } from "@emotion/react";
 
 const App = ({ Component, pageProps }) => (
-    <>
-        <ThemeProvider theme={lightTheme}>
-            {globalStyles}
-            <Component {...pageProps} />
-        </ThemeProvider>
-    </>
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
+      <meta name="description" content="Description" />
+      <meta name="keywords" content="Keywords" />
+      <title>Next.js PWA Example</title>
+
+      <link rel="manifest" href="/manifest.json" />
+      <link
+        href="/icons/favicon-16x16.png"
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+      />
+      <link
+        href="/icons/favicon-32x32.png"
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+      />
+      <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+      <meta name="theme-color" content="#317EFB" />
+    </Head>
+    <ThemeProvider theme={lightTheme}>
+      {globalStyles}
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
 );
 
 export default App;
