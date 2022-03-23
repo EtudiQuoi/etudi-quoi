29e911347a33ae9c3a709475507357d36ab885f5
MisterAzix <flizi@live.fr>
2022-03-23 15:12:07 +0100

    :sparkles: Adding Logo

---
+++ components/Logo.jsx
@@ -0,0 +1,20 @@
+import { ThemeContext } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export default function Logo() {
+    return (
+        <H1>
+            <span>Etudi'</span>Quoi
+        </H1>
+    );
+}
+
+const H1 = styled.h1`
+    font-size: 3rem;
+    font-weight: bold;
+    color: ${({ theme }) => theme.primaryHover};
+
+    span {
+        color: ${({ theme }) => theme.primary};
+    }
+`;
