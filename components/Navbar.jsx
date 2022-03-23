d1f5540255a537d089335be1956c2d4f40020318
MisterAzix <flizi@live.fr>
2022-03-23 15:12:19 +0100

    :sparkles: Adding Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,32 @@
 import styled from "@emotion/styled";
+import Link from "next/link";
+
+import CardIcon from "../icons/CardIcon";
+import ChartIcon from "../icons/ChartIcon";
 
 export default function Navbar() {
-    return <Nav>Navbar</Nav>;
+    return (
+        <Nav>
+            <NavItem href={"/"}>
+                <CardIcon active />
+            </NavItem>
+            <NavItem href={"/"}>
+                <ChartIcon />
+            </NavItem>
+        </Nav>
+    );
 }
 
 const Nav = styled.nav`
     display: flex;
+    align-items: center;
+    gap: 3rem;
+    padding: 0.75rem 2rem;
+    background: ${({ theme }) => theme.baseColor};
+    border-radius: 1rem;
+    box-shadow: ${({ theme }) => theme.boxShadow};
+`;
+
+const NavItem = styled(Link)`
+    cursor: pointer;
 `;
