647ef53eea57894ebfab7d274377efe90b1e96a5
MisterAzix <flizi@live.fr>
2022-03-23 15:11:46 +0100

    :sparkles: Update Range

--- components/Range.jsx
+++ components/Range.jsx
@@ -5,41 +5,12 @@ export default function Range(props) {
 
     return (
         <RangeContainer>
-            <Label>
-                {props.value}/{props.max} Propositions
-            </Label>
-            <RangeBox percent={percent} />
+            {props.value} Propositions restantes
         </RangeContainer>
     );
 }
 
-const RangeContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 0.5rem;
-`;
-
-const Label = styled.span`
+const RangeContainer = styled.span`
     font-weight: bold;
-    color: ${({ theme }) => theme.primary};
-`;
-
-const RangeBox = styled.div`
-    position: relative;
-    height: 8px;
-    width: 60%;
-    border-radius: 1rem;
-    background: ${({ theme }) => theme.primaryHover};
-
-    &:after {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        height: 100%;
-        width: ${(props) => props.percent <= 100 ? props.percent : 100}%;
-        border-radius: 1rem;
-        background: ${({ theme }) => theme.primary};
-    }
+    color: #27345A;
 `;
