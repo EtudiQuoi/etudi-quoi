b97bbb8fc5d3e2a4cfda103ca6e63315d6b190d3
NicoooM <nmarsan33000@gmail.com>
2022-04-05 21:55:33 +0200

    homepage created

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,20 +1,32 @@
 import { ThemeContext } from "@emotion/react";
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
-export default function Logo() {
+export default function Logo(props) {
     return (
-        <H1>
+        <H1 {...props}>
             <span>Etudi'</span>quoi
         </H1>
     );
 }
 
 const H1 = styled.h1`
-    font-size: 3rem;
     font-weight: bold;
     color: ${({ theme }) => theme.primaryHover};
 
     span {
         color: ${({ theme }) => theme.primary};
     }
+
+    ${(props) => {
+        if (props.size === "big") {
+            return css`
+                font-size: 4.5rem;
+            `;
+        } else {
+            return css`    
+                font-size: 3rem;
+            `;
+        }
+    }}
 `;
