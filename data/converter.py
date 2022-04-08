10506f007a634eaed7f0b348fa49146823c00052
MisterAzix <flizi@live.fr>
2022-04-08 12:59:21 +0200

    :sparkles: Include filter in converter

--- data/converter.py
+++ data/converter.py
@@ -1,54 +1,43 @@
 import xmltodict
 import json
-import xml.etree.ElementTree as ET
-import re
 
-config = json.load(open("./data/config.json", encoding='utf-8'))
+file_name = 'data'
+print('converting...')
 
-# with open('./data/export_fiches_RNCP.xml', 'r', encoding='utf-8') as myfile:
-#     obj = xmltodict.parse(myfile.read())
-# json_object = (json.dumps(obj, indent = 4))
+with open('{}.xml'.format(file_name), 'r', encoding='utf-8') as myfile:
+    obj = xmltodict.parse(myfile.read())
 
-root = ET.parse("./data/export_fiches_RNCP.xml").getroot()
+json_object = json.dumps(obj, indent = 4)
+json_output = json.loads(json_object)
 
-# print(root[1][1].text)
+new_json = {}
+new_json['FICHES'] = []
 
-idList = []
-questions = []
+def optional_chain(root, *keys):
+    result = root
+    for k in keys:
+        if isinstance(result, dict):
+            result = result.get(k, None)
+        else:
+            result = getattr(result, k, None)
+        if result is None:
+            break
+    return result
 
-# print(root["FICHE"])
+for fiche in json_output['FICHES']['FICHE']:
+    new_json['FICHES'].append({ 
+        'ID_FICHE': optional_chain(fiche, 'ID_FICHE'), 
+        'NUMERO_FICHE': optional_chain(fiche, 'NUMERO_FICHE'), 
+        'INTITULE': optional_chain(fiche, 'INTITULE'), 
+        'ABREGE': optional_chain(fiche, 'ABREGE'), 
+        'CODES_NSF': optional_chain(fiche, 'CODES_NSF'), 
+        'ACTIVITES_VISEES': optional_chain(fiche, 'ACTIVITES_VISEES'), 
+        'CAPACITES_ATTESTEES': optional_chain(fiche, 'CAPACITES_ATTESTEES'), 
+        'SECTEURS_ACTIVITE': optional_chain(fiche, 'SECTEURS_ACTIVITE'), 
+        'TYPE_EMPLOI_ACCESSIBLES': optional_chain(fiche, 'TYPE_EMPLOI_ACCESSIBLES'), 
+        'CODES_ROME': optional_chain(fiche, 'CODES_ROME'),
+        'BLOCS_COMPETENCES': optional_chain(fiche, 'BLOCS_COMPETENCES'),
+    })
 
-
-
-
-for object in config:
-    for keyword in object["keywords"]:
-        for fiche in root:
-            print(fiche[0].text)
-            for child in fiche:
-                if (child.text.find(keyword) != -1):
-                    print(keyword)
-                    print(child.text)
-                    if fiche[0].text not in idList:
-                        idList.append(fiche[0].text)
-                print("_______________")
-            print("----------------")
-    questions.append({"label": object.label, "id": idList})
-
-
-print(idList)
-print("_______________")
-print(questions)
-# with open("export.json", "w", encoding='utf-8') as outfile:
-#     outfile.write(json_object)
-
-    # 1 charger la config
-    #  charger le fichier de données
-    #  itérer sur les questions de la config
-    #  dans chaque questions, itérer sur chaque formation
-    #  on cherche s'il y a au moins un mot clé dans le texte
-    #  si non , on passe
-    #  si oui, on ajoute l'id de la formation dans le tableau de la questions
-    #  écrire un fichier avec les questions avec les formations et le code pour
-    #  itére sur les formations et on prépare les champs pour l'affichage (prendre les champs qui nous intéressent)
-
+with open("{}.json".format(file_name), "w", encoding='utf-8') as outfile:
+    json.dump(new_json, outfile, indent=4)
