f7136462112ea7fc7f016a3e44a901ac02747582
NicoooM <nmarsan33000@gmail.com>
2022-04-08 11:17:16 +0200

    finish tutorial

--- components/TutoSlide.jsx
+++ components/TutoSlide.jsx
@@ -5,27 +5,45 @@ import CardIcon from "../icons/CardIcon";
 import CardResult from "../components/CardResult";
 import SchoolCard from "../components/SchoolCard";
 
-const Slide1 = () => {
+const ContainerAnimation = (props) => {
+  if (props.direction === "right") {
+    return (
+      <Container
+        initial={{ x: 500, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: -500, opacity: 0 }}
+        transition={{ duration: 0.3, type: "spring" }}
+        {...props}
+      >
+        {props.children}
+      </Container>
+    );
+  } else {
+    return (
+      <Container
+        initial={{ x: -500, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: 500, opacity: 0 }}
+        transition={{ duration: 0.3, type: "spring" }}
+        {...props}
+      >
+        {props.children}
+      </Container>
+    );
+  }
+};
+
+const Slide1 = ({ direction }) => {
   return (
-    <motion.div
-      initial={{ x: 500, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      exit={{ x: -500, opacity: 0 }}
-      transition={{ duration: 0.3, type: "spring" }}
-    >
+    <ContainerAnimation css={{ alignItems: "center" }} direction={direction}>
       <CardIcon type="tutorial" />
-    </motion.div>
+    </ContainerAnimation>
   );
 };
 
-const Slide2 = () => {
+const Slide2 = ({ direction }) => {
   return (
-    <motion.div
-      initial={{ x: 500, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      exit={{ x: -500, opacity: 0 }}
-      transition={{ duration: 0.3, type: "spring" }}
-    >
+    <ContainerAnimation direction={direction}>
       <CardsList>
         <CardResult
           type="Bachelor Universitaire de Technologie"
@@ -40,18 +58,13 @@ const Slide2 = () => {
         />
       </CardsList>
       <H3>Tes formations apparaissent en fonction de tes centres d’intêrets</H3>
-    </motion.div>
+    </ContainerAnimation>
   );
 };
 
-const Slide3 = () => {
+const Slide3 = ({ direction }) => {
   return (
-    <Container
-      initial={{ x: 500, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      exit={{ x: -500, opacity: 0 }}
-      transition={{ duration: 0.3, type: "spring" }}
-    >
+    <ContainerAnimation direction={direction}>
       <h3>Métiers du Multimédia et de l'Internet</h3>
       <SchoolsList>
         <SchoolCard>IUT Bordeaux</SchoolCard>
@@ -59,16 +72,16 @@ const Slide3 = () => {
         <SchoolCard>IUT Toulon</SchoolCard>
         <SchoolCard>IUT Tours</SchoolCard>
       </SchoolsList>
-    </Container>
+    </ContainerAnimation>
   );
 };
 
-const TutoSlide = ({ slideNumber }) => {
+const TutoSlide = ({ slideNumber, direction }) => {
   return (
     <>
-      {slideNumber === 1 && <Slide1 />}
-      {slideNumber === 2 && <Slide2 />}
-      {slideNumber === 3 && <Slide3 />}
+      {slideNumber === 1 && <Slide1 direction={direction} />}
+      {slideNumber === 2 && <Slide2 direction={direction} />}
+      {slideNumber === 3 && <Slide3 direction={direction} />}
     </>
   );
 };
@@ -78,6 +91,7 @@ const CardsList = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 1rem;
+  width: 100vw;
 `;
 
 const Separator = styled.hr`
--- pages/tutorial.js
+++ pages/tutorial.js
@@ -1,55 +1,20 @@
 import styled from "@emotion/styled";
-import { css } from "@emotion/react";
-import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
 
 import { Container } from "../shared/styles";
 import Logo from "../components/Logo";
-import Button from "../components/Button";
-import TutoSlide from "../components/TutoSlide";
-
-import Link from "next/link";
+import TutorialContent from "../components/TutorialContent";
 
 const Tutorial = () => {
-  const [slideNumber, setSlideNumber] = useState(1);
-
-  const changeSlide = () => {
-    setSlideNumber(slideNumber + 1);
-  };
-
   return (
     <Container>
       <Grid>
         <GridItem area="header">
           <Logo />
         </GridItem>
-        <GridItem area="subtitle">
-          <H2>
-            {slideNumber === 1
-              ? "Swipe les propositions qui te caractérisent"
-              : slideNumber === 2
-              ? "Découvre les formations qui te correspondent"
-              : "Choisis la formation qui te convient"}
-          </H2>
-        </GridItem>
-        <GridItem>
-          <TutoSlide slideNumber={slideNumber} />
-        </GridItem>
-        <GridItem area="button">
-          <Bullets>
-            <Bullet active={slideNumber === 1} />
-            <Bullet active={slideNumber === 2} />
-            <Bullet active={slideNumber === 3} />
-          </Bullets>
-          {slideNumber < 3 ? (
-            <Button onClick={changeSlide}>Suivant</Button>
-          ) : (
-            <Link href={"/"}>
-              <a>
-                <Button>Lancer</Button>
-              </a>
-            </Link>
-          )}
-        </GridItem>
+        <AnimatePresence exitBeforeEnter>
+          <TutorialContent />
+        </AnimatePresence>
       </Grid>
     </Container>
   );
@@ -72,33 +37,6 @@ const GridItem = styled.div`
   grid-area: ${(props) => props.area};
   display: grid;
   place-items: center;
-`;
-
-const H2 = styled.h2`
-  font-size: 1.5rem;
-  padding: 0 5rem;
-`;
-
-const Bullets = styled.div`
-  display: flex;
-  gap: 2rem;
-`;
-
-const Bullet = styled.div`
-  height: 0.7rem;
-  width: 0.7rem;
-  border-radius: 1rem;
-  ${(props) => {
-    if (props.active) {
-      return css`
-        background-color: ${props.theme.primary};
-      `;
-    } else {
-      return css`
-        background-color: ${props.theme.primaryHover};
-      `;
-    }
-  }}
 `;
 
 export default Tutorial;
