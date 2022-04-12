1f64348b65f243cbb29b4259a64315490a936fa5
NicoooM <nmarsan33000@gmail.com>
2022-04-12 17:30:10 +0200

    v1 settings

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,38 +1,65 @@
 import styled from "@emotion/styled";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { css } from "@emotion/react";
+import { useState } from "react";
+
+import Setting from "./Setting";
 
 import CardIcon from "../icons/CardIcon";
 import ChartIcon from "../icons/ChartIcon";
+import SettingIcon from "../icons/SettingIcon";
 
 export default function Navbar() {
+  const [isOpenSetting, setIsOpenSetting] = useState(false);
   const router = useRouter();
 
+  const openSetting = () => {
+    setIsOpenSetting(true);
+  };
+
   return (
-    <Nav>
-      <Link href="/questions">
-        <A title="Swipe">
-          <CardIcon active={router.pathname === "/questions"} />
-          <Span>Cartes</Span>
-        </A>
-      </Link>
-      <Link href="/results">
-        <A title="Résultats">
-          <ChartIcon
-            active={
-              router.pathname === "/results" || router.pathname === "/ecoles"
-            }
-          />
-          <Span>Résultats</Span>
-        </A>
-      </Link>
-    </Nav>
+    <>
+      <Container>
+        <EmptyDiv />
+        <Nav>
+          <Link href="/questions">
+            <A title="Swipe">
+              <CardIcon active={router.pathname === "/questions"} />
+              <Span>Cartes</Span>
+            </A>
+          </Link>
+          <Link href="/results">
+            <A title="Résultats">
+              <ChartIcon
+                active={
+                  router.pathname === "/results" ||
+                  router.pathname === "/ecoles"
+                }
+              />
+              <Span>Résultats</Span>
+            </A>
+          </Link>
+        </Nav>
+        <SettingIcon onClick={openSetting} />
+      </Container>
+      {isOpenSetting && <Setting setIsOpenSetting={setIsOpenSetting} />}
+    </>
   );
 }
 
-const Nav = styled.nav`
+const Container = styled.div`
+  width: 100%;
   display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const EmptyDiv = styled.div`
+  width: 30px;
+`;
+
+const Nav = styled.nav`
+  display: inline-flex;
   align-items: center;
   gap: 3rem;
   padding: 0.75rem 2rem;
@@ -51,6 +78,13 @@ const A = styled.a`
   flex-direction: column;
   align-items: center;
   gap: 0.2rem;
+`;
+
+const SettingIconContainer = styled.div`
+  // position: absolute;
+  // right: 5%;
+  // top: 50%;
+  // transform: translateY(-50%);
 `;
 
 // const NavItem = styled(Link)`
--- shared/styles.js
+++ shared/styles.js
@@ -2,46 +2,48 @@ import { css, Global } from "@emotion/react";
 import styled from "@emotion/styled";
 
 export const lightTheme = {
-    primary: "#0C53A3",
-    primaryHover: "#84B9EB",
-    accentColor: "#E94057",
-    accentColorHover: "#F6A0AC",
-    darkColor: "#083262",
-    darkShade: "#161C2E",
-    baseColor: "#FFFFFF",
-    boxShadow: "0px 4px 16px rgba(52, 89, 131, 0.16)",
-    backgroundGradient: "linear-gradient(180deg, #FFFFFF 0%, #D6E0FF 100%);",
-    primaryGradient: "radial-gradient(100% 100% at 100% 100%, #A4D2FF 0%, #3C84C8 50.52%, #054999 100%)",
-    transition: "0.3s ease",
+  primary: "#0C53A3",
+  primaryHover: "#84B9EB",
+  accentColor: "#E94057",
+  accentColorHover: "#F6A0AC",
+  darkColor: "#083262",
+  darkShade: "#161C2E",
+  baseColor: "#FFFFFF",
+  boxShadow: "0px 4px 16px rgba(52, 89, 131, 0.16)",
+  backgroundGradient: "linear-gradient(180deg, #FFFFFF 0%, #D6E0FF 100%);",
+  primaryGradient:
+    "radial-gradient(100% 100% at 100% 100%, #A4D2FF 0%, #3C84C8 50.52%, #054999 100%)",
+  transition: "0.3s ease",
 };
 
 export const globalStyles = (
-    <Global
-        styles={css`
-            * {
-                padding: 0;
-                margin: 0;
-                box-sizing: border-box;
-            }
+  <Global
+    styles={css`
+      * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+      }
 
-            html,
-            body {
-                background: #171717;
-                min-height: 100%;
-                font-family: 'Epilogue', Helvetica, Arial, sans-serif;
-                font-size: 16px;
-            }
-        `}
-    />
+      html,
+      body {
+        background: #171717;
+        min-height: 100%;
+        font-family: "Epilogue", Helvetica, Arial, sans-serif;
+        font-size: 16px;
+      }
+    `}
+  />
 );
 
 export const Container = styled.div`
-    overflow: hidden;
-    margin: auto;
-    height: 100vh;
-    width: 100vw;
-    padding: 1rem 0;
-    max-width: 480px;
-    background: ${({ theme }) => theme.backgroundGradient};
-    text-align: center;
+  overflow: hidden;
+  margin: auto;
+  height: 100vh;
+  position: relative;
+  width: 100vw;
+  padding: 1rem 0;
+  max-width: 480px;
+  background: ${({ theme }) => theme.backgroundGradient};
+  text-align: center;
 `;
