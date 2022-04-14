488e6c580b4feb83eebf898a839452eabc98bc30
NicoooM <nmarsan33000@gmail.com>
2022-04-14 16:28:30 +0200

    comment api

--- pages/api/ecoles/[ecoleId].js
+++ pages/api/ecoles/[ecoleId].js
@@ -1,19 +1,19 @@
-import data from "../../../data/data.json";
+// import data from "../../../data/data.json";
 
-export default function handler(req, res) {
-  const { ecoleId } = req.query;
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  const formation = data.FICHES.filter(
-    (formation) => formation.ID_FICHE === ecoleId
-  );
-  const certificateurs = [];
+// export default function handler(req, res) {
+//   const { ecoleId } = req.query;
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   const formation = data.FICHES.filter(
+//     (formation) => formation.ID_FICHE === ecoleId
+//   );
+//   const certificateurs = [];
 
-  Object.values(formation[0].CERTIFICATEURS.CERTIFICATEUR).forEach((row) => {
-    certificateurs.push(row.NOM_CERTIFICATEUR);
-  });
-  res.status(200).json(certificateurs);
-}
+//   Object.values(formation[0].CERTIFICATEURS.CERTIFICATEUR).forEach((row) => {
+//     certificateurs.push(row.NOM_CERTIFICATEUR);
+//   });
+//   res.status(200).json(certificateurs);
+// }
