1ec0243362469cadac5b11c7711296fe3189a4e8
MisterAzix <flizi@live.fr>
2022-04-14 11:15:19 +0200

    :sparkles: Prevent useless formation

--- data/converter.py
+++ data/converter.py
@@ -32,21 +32,22 @@ counter = 0
 for fiche in json_output['FICHES']['FICHE']:
     # if optional_chain(fiche, 'BLOCS_COMPETENCES') is not None:
     if optional_chain(fiche, 'CODES_NSF') is not None:
-        new_json['FICHES'].append({ 
-            'ID_FICHE': optional_chain(fiche, 'ID_FICHE'), 
-            'NUMERO_FICHE': optional_chain(fiche, 'NUMERO_FICHE'), 
-            'INTITULE': optional_chain(fiche, 'INTITULE'), 
-            'NOMENCLATURE_EUROPE': optional_chain(fiche, 'NOMENCLATURE_EUROPE'), 
-            'ABREGE': optional_chain(fiche, 'ABREGE'), 
-            'CODES_NSF': optional_chain(fiche, 'CODES_NSF'), 
-            'CERTIFICATEURS': optional_chain(fiche, 'CERTIFICATEURS'), 
-            'ACTIVITES_VISEES': optional_chain(fiche, 'ACTIVITES_VISEES'), 
-            'CAPACITES_ATTESTEES': optional_chain(fiche, 'CAPACITES_ATTESTEES'), 
-            'SECTEURS_ACTIVITE': optional_chain(fiche, 'SECTEURS_ACTIVITE'), 
-            'TYPE_EMPLOI_ACCESSIBLES': optional_chain(fiche, 'TYPE_EMPLOI_ACCESSIBLES'), 
-            'CODES_ROME': optional_chain(fiche, 'CODES_ROME'),
-            'BLOCS_COMPETENCES': optional_chain(fiche, 'BLOCS_COMPETENCES'),
-        })
+        if not search('Sans niveau', optional_chain(fiche, 'NOMENCLATURE_EUROPE', 'INTITULE')):
+            new_json['FICHES'].append({ 
+                'ID_FICHE': optional_chain(fiche, 'ID_FICHE'), 
+                'NUMERO_FICHE': optional_chain(fiche, 'NUMERO_FICHE'), 
+                'INTITULE': optional_chain(fiche, 'INTITULE'), 
+                'NOMENCLATURE_EUROPE': optional_chain(fiche, 'NOMENCLATURE_EUROPE'), 
+                'ABREGE': optional_chain(fiche, 'ABREGE'), 
+                'CODES_NSF': optional_chain(fiche, 'CODES_NSF'), 
+                'CERTIFICATEURS': optional_chain(fiche, 'CERTIFICATEURS'), 
+                'ACTIVITES_VISEES': optional_chain(fiche, 'ACTIVITES_VISEES'), 
+                'CAPACITES_ATTESTEES': optional_chain(fiche, 'CAPACITES_ATTESTEES'), 
+                'SECTEURS_ACTIVITE': optional_chain(fiche, 'SECTEURS_ACTIVITE'), 
+                'TYPE_EMPLOI_ACCESSIBLES': optional_chain(fiche, 'TYPE_EMPLOI_ACCESSIBLES'), 
+                'CODES_ROME': optional_chain(fiche, 'CODES_ROME'),
+                'BLOCS_COMPETENCES': optional_chain(fiche, 'BLOCS_COMPETENCES'),
+            })
 
 with open("{}/{}.json".format(dir, file_name), "w", encoding='utf-8') as outfile:
     json.dump(new_json, outfile, indent=4)
